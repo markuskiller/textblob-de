0a60aea063201ed4462532cd5be1787dc514a1a0
Markus Killer <m.killer@langui.ch>
2014-07-09 23:24:07 +0200

    bumped version for dev branch

--- textblob_de/__init__.py
+++ textblob_de/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 from textblob_de.taggers import PatternTagger
 from textblob_de.sentiments import PatternAnalyzer
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __author__ = 'Markus Killer'
 __license__ = "MIT"
