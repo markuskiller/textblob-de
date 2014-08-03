7a4b4177b0413ea1a68f2c83605e61f8613e311e
Markus Killer <m.killer@langui.ch>
2014-08-04 00:46:41 +0200

    does not yet work for original version of pattern2.6 on Py2, removed requirements

--- setup.py
+++ setup.py
@@ -14,10 +14,8 @@ import re
 from setuptools import setup
 
 packages = ['textblob_de']
-if sys.version_info[0] == 2:
-    requires = ["textblob>=0.8.0", "pattern>=2.6.0"]
-else:
-    requires = ["textblob>=0.8.0"]
+requires = ["textblob>=0.8.0"]
+
 
 PUBLISH_CMD = "python setup.py register sdist bdist_wheel upload"
 TEST_PUBLISH_CMD = 'python setup.py register -r test sdist bdist_wheel upload -r test'
